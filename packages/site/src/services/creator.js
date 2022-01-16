b43b5b5116719af54c2847bbf4b60cb3bf7b3bb1
khairaf <khairanif999@gmail.com>
2022-01-16 17:41:13 +0700

    Search creator using backend api

--- packages/site/src/services/creator.js
+++ packages/site/src/services/creator.js
@@ -1,4 +1,6 @@
 import { api } from './api'
 
 export const fetchCreatorByUsername = (username) => api.get(`/creator/${username}`)
+export const fetchAllCreator = (query) => api.get('/creator', query)
+
 export const followOtherCreator = (id) => api.put(`/creator/${id}/follow`)
